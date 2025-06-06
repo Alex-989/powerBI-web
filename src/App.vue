35f4e0a49cbab626c7f93e724ca38be2471ce3d0
Alejandro <yoloyolonana1234@gmail.com>
2025-06-06 17:01:57 -0600

    Logro: Reporte PowerBI Web (Actualizado)

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
 <template>
   <div class="container">
         <h1>Reporte de Power BI</h1>
-        <p>Visualización interactiva de datos generada en Power BI desarrollada por Alejandr Daniel Aguilar Hernandez
-            datos generada en Power BI.</p>
+        <h3>Visualización interactiva de datos generada en Power BI desarrollada por Alejandro Daniel Aguilar Hernandez
+        </h3>
+        <p>Datos generada en Power BI.</p>
         <div class="iframe-container"> 
 
                 <iframe title="Visualizacion Datamart_Northwind2" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMTlmN2RhZmMtZTdiZC00Y2MyLThhMmUtN2RjMjUyM2RiZmEzIiwidCI6Ijg1ZjgyODhiLTNmZmYtNDkzZS1hYWZjLWE0NWVhYjA2YzM1MiJ9" frameborder="0" allowFullScreen="true"></iframe>
